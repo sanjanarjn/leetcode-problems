9df6f787eca9070958080af3f01e3e6f8c837baa
Sanjana Rajan <sanjanarjn@gmail.com>
2022-07-06 21:19:40 +0530

    Time: 0 ms (100.00%), Space: 41 MB (37.35%) - LeetHub

---
+++ 343-integer-break/343-integer-break.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public int integerBreak(int n) {
+        
+       if(n==2)
+            return 1;
+        if(n==3)
+            return 2;
+        int x;
+        x=n/3;
+        if(n%3==0)
+            return (int)Math.pow(3,x);
+        if(n%3==1)
+            return (int)Math.pow(3,x-1)*4;
+        return (int)Math.pow(3,x)*2;
+    }
+}
+
+// 5 - 6
+// 6 - 9
+// 7 - 12, 
+// 8 - 18, 
+// 9 - 27, 
+// 10 - 36
+// 11 - 54
+// 12 - 81
+// 13 - 108
+// 14 - 162
