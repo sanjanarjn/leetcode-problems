d31f79db736ce87445fb380a0a9edf74d919f20b
Sanjana Rajan <sanjanarjn@gmail.com>
2022-07-12 20:23:32 +0530

    Time: 214 ms (39.25%), Space: 42.4 MB (29.92%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -2,60 +2,60 @@ import java.util.HashMap;
 import java.util.Collections;
 
 class Solution {
-    public List<Integer> nums;
-    public int[] sums;
-    public int possibleSquareSide;
+        int n;
+    int[] matchsticks;
+    int[] edges;
 
-    public Solution() {
-        this.sums = new int[4];
+
+
+    public boolean makesquare(int[] matchsticks) {
+
+        
+        this.n = matchsticks.length;
+        this.matchsticks = Arrays.stream(matchsticks).boxed()
+            .sorted(Collections.reverseOrder())
+            .mapToInt(Integer::intValue)
+            .toArray();
+        
+        
+        int sum = 0;
+        for(int match : matchsticks) {
+            sum += match;
+        }
+
+        if(sum % 4 != 0)
+            return false;
+
+        int edge = sum/4;
+        this.edges = new int[] {edge, edge, edge, edge};
+
+        return partition(0, sum);
     }
 
-    // Depth First Search function.
-    public boolean dfs(int index) {
+    private boolean partition(int i, int totalSum) {
 
-        // If we have exhausted all our matchsticks, check if all sides of the square are of equal length
-        if (index == this.nums.size()) {
-            return sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
+        if(i == n)
+            return totalSum == 0;
+
+        if(totalSum == 0)
+            return false;
+
+        int elem = matchsticks[i];
+        for(int j = 0; j < edges.length; j++) {
+
+            if(elem > edges[j])
+                continue;
+
+            edges[j] -= elem;
+            totalSum -= elem;
+
+            boolean canPartition = partition(i + 1, totalSum);
+            if(canPartition)
+                return true;
+
+            edges[j] += elem;
+            totalSum += elem;
         }
-
-        // Get current matchstick.
-        int element = this.nums.get(index);
-
-        // Try adding it to each of the 4 sides (if possible)
-        for(int i = 0; i < 4; i++) {
-            if (this.sums[i] + element <= this.possibleSquareSide) {
-                this.sums[i] += element;
-                if (this.dfs(index + 1)) {
-                    return true;
-                }
-                this.sums[i] -= element;
-            }
-        }
-
         return false;
-    }
-
-    public boolean makesquare(int[] nums) {
-        // Empty matchsticks.
-        if (nums == null || nums.length == 0) {
-            return false;
-        }
-
-        // Find the perimeter of the square (if at all possible)
-        int L = nums.length;
-        int perimeter = 0;
-        for(int i = 0; i < L; i++) {
-            perimeter += nums[i];
-        }
-
-        this.possibleSquareSide =  perimeter / 4;
-        if (this.possibleSquareSide * 4 != perimeter) {
-            return false;
-        }
-
-        // Convert the array of primitive int to ArrayList (for sorting).
-        this.nums = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        Collections.sort(this.nums, Collections.reverseOrder());
-        return this.dfs(0);
     }
 }
