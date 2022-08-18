be1115211846581e28433c8712fd69dcec16b6cc
Sanjana Rajan <sanjanarjn@gmail.com>
2022-08-18 10:36:58 +0530

    Time: 16 ms (29.49%), Space: 45.9 MB (24.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,26 +1,14 @@
 class Solution {
+    
     public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length())
+            return false;
+        
         Map<Character, Integer> sCount = getCount(s);
         Map<Character, Integer> tCount = getCount(t);
         
-        Map<Character, Integer> largerMap = sCount;
-        Map<Character, Integer> smallerMap = tCount;
-        if(tCount.size() > sCount.size()) {
-            largerMap = tCount;
-            smallerMap = sCount;
-        }
-
-         for (char key : largerMap.keySet()) {
-            if (!smallerMap.containsKey(key))
-                return false;
-
-            int count1 = smallerMap.get(key);
-            int count2 = largerMap.get(key);
-            if (count1 != count2)
-                return false;
-        }
-        
-        return true;
+        return sCount.equals(tCount);
         
     }
     
